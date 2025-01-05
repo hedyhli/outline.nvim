14e245bbfa6146ad248ae1041bdac4fff274b1a6
eph <eph@MacBook-Pro.local>
2025-01-06 00:27:33 +0800

    feat: add to jumplist before goto location

--- lua/outline/sidebar.lua
+++ lua/outline/sidebar.lua
@@ -392,6 +392,9 @@ function Sidebar:__goto_location(change_focus)
     return
   end
 
+  -- XXX: There will be strange problems when using `nvim_buf_set_mark()`.
+  vim.fn.win_execute(self.code.win, "normal! m'")
+
   vim.api.nvim_win_set_cursor(self.code.win, { node.line + 1, node.character })
 
   if cfg.o.outline_window.center_on_jump then
